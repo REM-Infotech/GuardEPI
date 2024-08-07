0f801aace3ff2da534ed2dcf3c60c4d9288c8cb6
Robotz213 <nicholasazs213@gmail.com>
2024-08-07 08:50:47 -0400

    csp.json

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
+import json
 
 def debugmode() -> bool:
 
@@ -12,48 +12,5 @@ def debugmode() -> bool:
 
 def csp() -> dict[str]:
 
-    return {
-        'default-src': [
-            '\'self\'',
-        ],
-        'script-src': [
-            '\'self\'',
-            'https://cdn.jsdelivr.net',
-            'https://cdnjs.cloudflare.com',
-            'https://cdn.datatables.net',
-            'https://unpkg.com',
-            'https://code.jquery.com',
-            'https://use.fontawesome.com',
-            '',
-            '\'unsafe-inline\'',  # Permite scripts inline
-        ],
-        'style-src': [
-            '\'self\'',
-            'https://cdn.jsdelivr.net',
-            'https://cdnjs.cloudflare.com',
-            'https://cdn.datatables.net',
-            'https://unpkg.com',
-            '\'unsafe-inline\'',
-        ],
-        'img-src': [
-            '\'self\'',
-            'data:',
-            'https://cdn.jsdelivr.net',
-            'https://cdnjs.cloudflare.com',
-            'https://cdn.datatables.net',
-            'https://unpkg.com',
-            'https://cdn-icons-png.flaticon.com',
-            'http://guardepi.robotz.dev',
-        ],
-        'connect-src': [
-            '\'self\'',
-            'https://cdn.jsdelivr.net',
-            'https://cdnjs.cloudflare.com',
-            'https://cdn.datatables.net',
-            'https://unpkg.com',
-        ],
-        'frame-src': [
-            '\'self\'',
-            'http://guardepi.robotz.dev',
-        ]
-    }
+    csp_vars = json.load(os.path.join(os.getcwd(), "configs", "csp.json"))
+    return csp_vars
