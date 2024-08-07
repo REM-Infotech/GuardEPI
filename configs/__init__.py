d7a7055384f225c092843e6e907406142a972560
Robotz213 <nicholasazs213@gmail.com>
2024-08-07 10:21:31 -0400

    att

--- configs/__init__.py
+++ configs/__init__.py
@@ -5,12 +5,56 @@ import json
 
 def debugmode() -> bool:
 
-    debug = os.getenv("DEBUG", "False").lower() in (
-        "true", "1", "t", "y", "yes")
+    debug = os.getenv('DEBUG', 'False').lower() in (
+        'true', '1', 't', 'y', 'yes')
     return debug
 
 
 def csp() -> dict[str]:
 
-    csp_vars = json.load(os.path.join(os.getcwd(), "configs", "csp.json"))
+    csp_vars = {
+    'default-src': [
+        '\'self\''
+    ],
+    'script-src': [
+        '\'self\'',
+        'https://cdn.jsdelivr.net',
+        'https://cdnjs.cloudflare.com',
+        'https://cdn.datatables.net',
+        'https://unpkg.com',
+        'https://code.jquery.com',
+        'https://use.fontawesome.com',
+        '',
+        '\'unsafe-inline\'',
+    ],
+    'style-src': [
+        '\'self\'',
+        'https://cdn.jsdelivr.net',
+        'https://cdnjs.cloudflare.com',
+        'https://cdn.datatables.net',
+        'https://unpkg.com',
+        '\'unsafe-inline\'',
+    ],
+    'img-src': [
+        '\'self\'',
+        'data:',
+        'https://cdn.jsdelivr.net',
+        'https://cdnjs.cloudflare.com',
+        'https://cdn.datatables.net',
+        'https://unpkg.com',
+        'https://cdn-icons-png.flaticon.com',
+        'http: //guardepi.robotz.dev',
+    ],
+    'connect-src': [
+        '\'self\'',
+        'https://cdn.jsdelivr.net',
+        'https://cdnjs.cloudflare.com',
+        'https://cdn.datatables.net',
+        'https://unpkg.com',
+    ],
+    'frame-src': [
+        '\'self\'',
+        'https://guardepi.robotz.dev',
+    ]
+}
     return csp_vars
--- src/js/DataTables/DashboardTable.js
+++ src/js/DataTables/DashboardTable.js
@@ -1,7 +1,4 @@
 window.addEventListener('DOMContentLoaded', event => {
-    // Simple-DataTables
-    // https://github.com/fiduswriter/Simple-DataTables/wiki
-
     const datatablesSimple = document.getElementById('DashboardTable');
     if (datatablesSimple) {
         new DataTable(datatablesSimple);
