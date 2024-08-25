9713e8ad5de6f504c9bd2cc9f0a38671b57330d4
Robotz213 <nicholasazs213@gmail.com>
2024-08-24 22:31:38 -0400

    att

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import subprocess
 import sys
 from dotenv import load_dotenv
 
+
 def install_cloudflared() -> str:
-    
+
     os_name = platform.system().lower()
     print("Detectado Linux.")
     url = "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb"
@@ -21,22 +22,26 @@ def install_cloudflared() -> str:
     print("Cloudflared instalado com sucesso.")
     return binary_name
 
+
 def configure_tunnel(token: str, binary_name: str):
     print("Configurando o Cloudflared Tunnel...")
     subprocess.run([f'{binary_name}', 'service', 'install', token])
 
+
 if __name__ == "__main__":
-    
+
     if platform.system() == "Linux":
         load_dotenv()
         token: str = os.getenv("CLOUDFLARED_TOKEN")
-        
-        if not token:
-            print("Token não encontrado. Verifique se o arquivo .env está configurado corretamente.")
 
-        configure_tunnel(token, install_cloudflared())
-        
+        if token:
+            configure_tunnel(token, install_cloudflared())
+
+        else:
+            print(
+                "Token não encontrado. Verifique se o arquivo .env está configurado corretamente.")
+
     from app import app
-    from configs import * 
-    
+    from configs import *
+
     app.run("0.0.0.0", 5002, debugmode())
