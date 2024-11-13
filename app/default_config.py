d87a468105f22053796d0f52c655b73a9ef489b2
Robotz213 <nicholas@robotz.dev>
2024-11-12 21:36:54 -0400

    [Titulo]: Change SQL Mysql to PgSQL

--- app/default_config.py
+++ app/default_config.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
+from pathlib import Path
 from uuid import uuid4
-import platform
+import shutil
 import os
 
 from datetime import timedelta
@@ -13,6 +14,7 @@ host_db = os.getenv("host")
 database_name = os.getenv("database")
 
 # PARAMETROS PARA O APP FLASK
+DEBUG = True
 PDF_PATH = os.path.join(os.getcwd(), "PDF")
 DOCS_PATH = os.path.join(os.getcwd(), "Docs")
 TEMP_PATH = os.path.join(os.getcwd(), "Temp")
@@ -20,37 +22,30 @@ IMAGE_TEMP_PATH = os.path.join(TEMP_PATH, "IMG")
 CSV_TEMP_PATH = os.path.join(TEMP_PATH, "csv")
 PDF_TEMP_PATH = os.path.join(TEMP_PATH, "pdf")
 
-debug = os.getenv("DEBUG", "False").lower() in ("true", "1", "t", "y", "yes")
 
-database_uri = f"mysql://{login_db}:{passwd_db}@{host_db}/{database_name}"
-if debug is True:
-    database_uri = "sqlite:///project.db"
+"""SqlAlchemy Config"""
+SQLALCHEMY_POOL_SIZE = 30  # Número de conexões na pool
+SQLALCHEMY_MAX_OVERFLOW = 10  # Número de conexões extras além da pool_size
+SQLALCHEMY_POOL_TIMEOUT = 30  # Tempo de espera para obter uma conexão
+SQLALCHEMY_POOL_RECYCLE = 1800  # Tempo (em segundos) para reciclar as conexões ociosas
+SQLALCHEMY_POOL_PRE_PING = True  # Verificar a saúde da conexão antes de usá-la
 
-SQLALCHEMY_DATABASE_URI = database_uri
-
+SQLALCHEMY_DATABASE_URI = (
+    f"postgresql+psycopg2://{login_db}:{passwd_db}@{host_db}:5432/{database_name}"
+)
+SQLALCHEMY_BINDS = {"cachelogs": "sqlite:///cachelogs.db"}
+SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+"""FLASK CONFIG"""
 PREFERRED_URL_SCHEME = "https"
 SESSION_COOKIE_HTTPONLY = False
 SESSION_COOKIE_SECURE = True
-PERMANENT_SESSION_LIFETIME = timedelta(days=7).max.seconds
-SRC_IMG_PATH = os.path.join(os.getcwd(), "app", "src", "assets", "img")
+PERMANENT_SESSION_LIFETIME = timedelta(days=31).max.seconds
 SECRET_KEY = str(uuid4())
 
 for paths in [DOCS_PATH, TEMP_PATH, IMAGE_TEMP_PATH, CSV_TEMP_PATH, PDF_TEMP_PATH]:
 
-    if not os.path.exists(paths):
-        os.makedirs(paths, exist_ok=True)
-
-    else:
-
-        plataforma = platform.system()
-
-        if plataforma == "Linux":
-            path = f"{paths}/*"
-            comand = "rm -r " + path
-
-        elif plataforma == "Windows":
-            path = f"{paths}\\*"
-            command = "powershell rm -r " + path
-
-        os.system(command)
+    shutil.rmtree(paths)
+    Path(paths).mkdir(exist_ok=True)
