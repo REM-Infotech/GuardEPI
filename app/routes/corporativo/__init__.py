13d9deee6062d8a2a94a4461f186dc400cfbe64b
Robotz213 <nicholas@robotz.dev>
2024-12-18 16:11:16 -0400

    [TSK-276] refactor: remove unused result divs from multiple EPI and corporativo templates for cleaner markup

--- app/routes/corporativo/__init__.py
+++ app/routes/corporativo/__init__.py
@@ -1,13 +1,26 @@
+from importlib import import_module
 from pathlib import Path
 
-from flask import Blueprint
-
-from . import cargos, departamentos, empresas, funcionarios
+from flask import Blueprint, redirect, url_for
 
 template_folder = Path(__file__).parent.resolve().joinpath("templates")
 corp = Blueprint(
-    "corp", __name__, template_folder=template_folder, prefix="/corporativo"
+    "corp", __name__, template_folder=template_folder, url_prefix="/corporativo"
 )
 
 
-__all__ = [cargos, empresas, funcionarios, departamentos]
+@corp.get("/")
+def redirecting():
+
+    return redirect(url_for("corp.Empresas"))
+
+
+if corp is not None:
+
+    def corp_bp():
+        import_module(".cargos", package=__package__)
+        import_module(".departamentos", package=__package__)
+        import_module(".empresas", package=__package__)
+        import_module(".funcionarios", package=__package__)
+
+    corp_bp()
