aa9f320e9e2fde4c36f223918eff5230a4319c66
Robotz213 <nicholas@robotz.dev>
2025-01-02 16:07:05 -0400

    [TSK-276] refactor: enhance model documentation for ProdutoEPI and related classes

--- app/models/EPI/equipamento.py
+++ app/models/EPI/equipamento.py
@@ -6,6 +6,30 @@ from app import db
 
 
 class ProdutoEPI(db.Model):
+    """
+    ProdutoEPI represents a model for personal protective equipment (PPE) products.
+    Attributes:
+        id (int): Unique identifier for the PPE product.
+        ca (str): Certificate of Approval (CA) number for the PPE product.
+        cod_ca (int): Code associated with the CA number.
+        nome_epi (str): Name of the PPE product.
+        tipo_epi (str): Type/category of the PPE product.
+        valor_unitario (float): Unit price of the PPE product.
+        qtd_entregar (int): Quantity to be delivered.
+        periodicidade_item (int): Periodicity of the item, default is 10.
+        fornecedor (str): Supplier of the PPE product.
+        marca (str): Brand of the PPE product.
+        modelo (str): Model of the PPE product.
+        filename (str): Filename associated with the PPE product.
+        blob_doc (bytes): Binary large object (BLOB) document associated with the PPE product.
+        vencimento (datetime): Expiration date of the PPE product, default is current datetime in GMT+4 timezone.
+        descricao (str): Description of the PPE product, default is "Sem Descrição".
+    Methods:
+        __init__(*args: tuple, **kwargs: dict) -> None:
+            Initializes a new instance of the ProdutoEPI class.
+        __getattr__(self, attr: str):
+            Retrieves the attribute specified by 'attr'. Raises an AttributeError if the attribute is not found.
+    """
 
     __tablename__ = "produto_epi"
     id: int = db.Column(db.Integer, primary_key=True, unique=True)
@@ -26,45 +50,72 @@ class ProdutoEPI(db.Model):
     )
     descricao: str = db.Column(db.Text, default="Sem Descrição")
 
-    def __init__(self, *args: tuple, **kwargs: dict) -> None:  # pragma: no cover
-        super().__init__(*args, **kwargs)
-
-    def __getattr__(self, attr: str):  # pragma: no cover
-
-        func = super().__getattr__(attr)
-        if not func:
-            func = getattr(self, attr)
-            if not func:
-                raise AttributeError(f"Attribute {attr} not found")
-
-        return func
-
 
 class Fornecedores(db.Model):
+    """
+    Fornecedores model represents suppliers in the database.
+    Attributes:
+        id (int): Unique identifier for the supplier.
+        fornecedor (str): Name of the supplier, must be unique and not null.
+        descricao (str): Description of the supplier.
+    """
+
     id = db.Column(db.Integer, primary_key=True, unique=True)
     fornecedor = db.Column(db.String(length=64), nullable=False, unique=True)
     descricao = db.Column(db.String(length=512))
 
 
 class Marcas(db.Model):
+    """
+    Marcas model represents the brands of equipment in the database.
+    Attributes:
+        id (int): Unique identifier for the brand.
+        marca (str): Name of the brand, must be unique and not null.
+        descricao (str): Description of the brand, optional.
+    """
+
     id = db.Column(db.Integer, primary_key=True, unique=True)
     marca = db.Column(db.String(length=64), nullable=False, unique=True)
     descricao = db.Column(db.String(length=512))
 
 
 class ModelosEPI(db.Model):
+    """
+    Model representing the EPI (Personal Protective Equipment) models.
+    Attributes:
+        id (int): The unique identifier for the EPI model.
+        modelo (str): The name of the EPI model, must be unique and not null.
+        descricao (str): A description of the EPI model.
+    """
+
     id = db.Column(db.Integer, primary_key=True, unique=True)
     modelo = db.Column(db.String(length=64), nullable=False, unique=True)
     descricao = db.Column(db.String(length=512))
 
 
 class ClassesEPI(db.Model):
+    """
+    Represents a class of Personal Protective Equipment (PPE).
+    Attributes:
+        id (int): The unique identifier for the PPE class.
+        classe (str): The name of the PPE class.
+        descricao (str): A description of the PPE class.
+    """
+
     id = db.Column(db.Integer, primary_key=True, unique=True)
     classe = db.Column(db.String(length=64), nullable=False, unique=True)
     descricao = db.Column(db.String(length=512))
 
 
 class GradeEPI(db.Model):
+    """
+    Represents a GradeEPI model for the database.
+    Attributes:
+        id (int): The primary key for the GradeEPI.
+        grade (str): The grade of the EPI, must be unique and not null.
+        descricao (str): A description of the EPI grade.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     grade = db.Column(db.String(length=32), nullable=False, unique=True)
     descricao = db.Column(db.String(length=512))
