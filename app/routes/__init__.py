0598f6d80b5722d770ab9a5b1b835bba634bb102
Robotz213 <nicholas@robotz.dev>
2025-01-21 02:37:25 +0000

    fix: handle missing error description in internal server error response

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -52,7 +52,9 @@ def register_routes(app: Flask) -> None:
         # desc = tradutor.translate(error.description)
 
         name: str = "Erro interno"
-        if error.code == 500 and "já cadastrado" not in error.desc:
+        if error.code == 500 and "já cadastrado" not in getattr(
+            error, "desc", "Erro do sistema"
+        ):
             desc: str = "Erro do sistema"
 
         if error.code == 405:
